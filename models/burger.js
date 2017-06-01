e9f00db7c25a8a51d744a7a6591a532914616fbd
Dan Phillips <dlphillips@gmail.com>
2017-06-01 13:48:19 -0400

    ready for submission

--- models/burger.js
+++ models/burger.js
@@ -6,7 +6,7 @@ var burger = {
       cb(res);
     });
   },
-  // The variables cols and vals are arrays.
+
   create: function(cols, vals, cb) {
     orm.create("burgers", cols, vals, function(res) {
       cb(res);
@@ -17,16 +17,8 @@ var burger = {
       cb(res);
     });
   },
-  delete: function(condition, cb) {
-    orm.delete("burgers", condition, function(res) {
-      cb(res);
-    });
-  }
 };
 
-// Export the database functions for the controller (burgers_Controller.js).
+// Export the database functions for the controller (burgers_controller.js).
 module.exports = burger;
 
-
-// Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
-// Export at the end of the burger.js file.
